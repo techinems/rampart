148a34d9f0ebf633ff0b915f99f5ddb296c0f5ed
Justin Etzine <justetz@gmail.com>
2019-10-19 19:11:54 -0700

    Added get single user route

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,16 @@ const express = require('express');
 module.exports = db => {
     const router = express.Router();
 
-    router.get('/', async (req, res) => {
-        res.json(await db.model('users').findAll());
+    router.get('/', async (req, res) => res.json(await db.model('users').findAll()));
+
+    router.get('/:userId', async (req, res) => {
+        console.log(req.params.userId);
+        res.json(await db.model('users').findOne({ where: { id: req.params.userId }}));
     });
+
+    // TODO: post to create a user
+    // TODO: put to update a user
+    // TODO: delete to delete a user
 
     return router;
 };
