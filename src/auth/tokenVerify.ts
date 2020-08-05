33446a74efffe214e6ee81b2f78d8ef559cc85f7
Logan <lramos15@gmail.com>
2020-08-05 10:45:33 -0400

    Some JWT work

---
+++ src/auth/tokenVerify.ts
@@ -0,0 +1,61 @@
+import  { Response, Request, NextFunction } from "express";
+import * as jwt from "jsonwebtoken";
+
+type JWT = {
+    permissions: string[];
+    [key: string]: string | number | string[] | number[];
+};
+
+export function permissionsMiddleware(req: Request, res: Response, next: NextFunction): void {
+    // Probably should use a regex to parse the main part of the url out
+    let parsedOriginalUrl = req.originalUrl.substring(1);
+    const slash_index = parsedOriginalUrl.indexOf("/");
+    if (slash_index !== -1) parsedOriginalUrl = parsedOriginalUrl.substring(0, parsedOriginalUrl.indexOf("/"));
+    const permission = parsedOriginalUrl + "." + methodToPermission(req.method);
+    const token: string | undefined = req.headers.jwt as string | undefined;
+    if (token === undefined) {
+        res.status(403);
+        res.send({
+            errorMessage: "No JWT specified!"
+        });
+        return;
+    }
+    if (process.env.JWT_SECRET === undefined) {
+        res.status(503);
+        res.send({
+            errorMessage: "JWT Secret not specified as environment variable!"
+        });
+        return;
+    }
+    try {
+        const parsedToken = jwt.verify(token, process.env.JWT_SECRET) as JWT;
+        if (!parsedToken.permissions.includes(permission)) {
+            res.status(503);
+            res.send({
+                errorMessage: `JWT does not contain ${permission} which is required for this endpoint!`
+            });
+            return;
+        }
+    } catch(err) {
+        res.status(400);
+        res.send({
+            errorMessage: "Unable to verify JWT!"
+        });
+        return;
+    }
+    next();
+}
+
+/**
+ * @description Converts http methods to the permissions which are needed to execute
+ * @param method The http method
+ * @returns A string containing the permission
+ */
+function methodToPermission(method: string): string {
+    if (method === "GET") return "read";
+    if (method === "PUT") return "write";
+    if (method === "POST") return "update";
+    if (method === "DELETE") return "delete";
+    // If it hits this case it means it's a method we don't know
+    return "";
+}
--- src/userRouter.ts
+++ src/userRouter.ts
@@ -17,3 +17,15 @@ userRouter.put("/", async (req: express.Request, res: express.Response) => {
         res.send(err);
     }
 });
+
+userRouter.get("/:id", async (req: express.Request, res: express.Response) => {
+    const id: number = parseInt(req.params.id);
+    try {
+        const user = await User.query().findById(id);
+        res.send(user);
+    } catch(err) {
+        // If there's not a status code in the error we go with 400
+        res.status(err.statusCode ?? 400);
+        res.send(err);
+    }
+});
