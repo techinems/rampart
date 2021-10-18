0258c0ec69c63a451a5a5045ca18b1ca9c5d2075
lramos15 <lramos15@gmail.com>
2021-10-18 00:57:09 +0000

    Update to safer query

--- src/routers/metadataRouter.ts
+++ src/routers/metadataRouter.ts
@@ -7,7 +7,12 @@ export const metadataRouter = Router();
 metadataRouter.get("/isNewUser/:id", async (req: Request, res: Response) => {
   const id = req.params.id;
   try {
-    const user: User | undefined = (await User.query().where('id', id).orWhere('g_id', id))[0];
+    let user: User | undefined = undefined;
+    if (parseInt(id) < Math.pow(2, 31)) {
+      user = (await User.query().where('id', id).orWhere('g_id', id))[0];
+    } else {
+      user = (await User.query().where('g_id', id))[0];
+    }
     res.send({ isNewUser: user === undefined });
   } catch (err: any) {
     // If there's not a status code in the error we go with 400
